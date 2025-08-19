0577d3dcd656be3bb923947c37b310401079d293
jorgeCasalini <joshcasalini@gmail.com>
2025-08-19 18:38:40 -0300

    feat: update text content in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const About = () => {
     return (
         <div id="sobre" className="pb-5" style={{ backgroundColor: "#f2f2f2" }}>
-            <h1 className="title-KoPub mt-1 mb-2" data-aos="fade-up"  data-aos-duration="1000">Sobre nossa empresa</h1>
-            <p className="text-center fs-5 mx-5 text-Ligconsolata" data-aos="zoom-in"  data-aos-duration="2000">Explore as águas cristalinas de Caldas Novas conosco! Nossa empresa de aluguel de lanchas e jet skis oferece uma experiência inesquecível para os amantes de aventura aquática. Histórias de momentos emocionantes e relaxantes em um dos destinos mais belos do Brasil. Com uma frota moderna e bem-mantida, garantimos a segurança e o conforto de nossos clientes. Nossa equipe experiente está pronta para ajudar a tornar sua experiência única e exclusiva. Aventure-se conosco e descubra a beleza natural de Caldas Novas de uma perspectiva totalmente nova!</p>
+            <h1 className="title-KoPub mt-1 mb-2" data-aos="fade-up" data-aos-duration="1000">Quem somos nós?</h1>
+            <p className="text-center fs-5 mx-5 text-Ligconsolata" data-aos="zoom-in" data-aos-duration="2000">A HLBoats surgiu em 2018 com um objetivo claro: proporcionar experiências inesquecíveis a bordo de lanchas privativas de alto conforto. Desde então, nos dedicamos incansavelmente a criar momentos incriveis para nossos clientes, oferecendo o melhor em aluguel de lanchas e jet skis no famoso lago Corumbá.</p>
         </div>
     );
 };
